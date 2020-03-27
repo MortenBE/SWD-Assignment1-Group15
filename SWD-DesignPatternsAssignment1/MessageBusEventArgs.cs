c714a0a92d5dad83cae67719bc39ee3e6f74bbba
MortenBE <Ehlersmorten@gmail.com>
2020-03-27 10:37:30 +0100

    Added messagebus

---
+++ SWD-DesignPatternsAssignment1/MessageBusEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWD_DesignPatternsAssignment1
+{
+    public class MessageBusEventArgs<T> : EventArgs
+    {
+        private T _message;
+
+        public MessageBusEventArgs(T Message)
+        {
+            _message = Message;
+        }
+
+        public T Message
+        {
+            get
+            {
+                return _message;
+            }
+        }
+    }
+}
---
+++ SWD-DesignPatternsAssignment1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SWD_DesignPatternsAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
